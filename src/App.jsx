b4b5c779ad49d94ee7181614d59718f245d603ea
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2023-12-09 18:49:09 +0100

    keep work on player component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import GameOver from "./components/GameOver";
+import { useState } from "react";
 
 const PLAYERS = {
   X: "Player 1",
@@ -14,13 +15,19 @@ const INITIAL_GAME_BOARD = [
   [null, null, null],
 ];
 
+const [names, setNames] = useState(PLAYERS);
+
+function handleNameChange(symbol, newName) {
+  setNames((currNames) => (currNames[symbol] = newName));
+}
+
 function App() {
   return (
     <main>
       <div id="game-container">
-        <ol id="players" >
-          <Player name={PLAYERS.X} symbol="X" />
-          <Player name={PLAYERS.O} symbol="O" />
+        <ol id="players">
+          <Player name={PLAYERS.X} symbol="X" onNameChange={handleNameChange} />
+          <Player name={PLAYERS.O} symbol="O" onNameChange={handleNameChange} />
         </ol>
         <GameBoard />
         <Log />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
-export default function Player({ name, symbol }) {
+import { useState } from "react";
+export default function Player({ name, symbol, onNameChange }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleSetIsEditing() {
+    setIsEditing((currStatus) => !currStatus);
+  }
+
+  function getName(event){
+    return event.target.value;
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (<input defaultValue={name} />) 
+        : (<span className="player-name">{name}</span>)}
         <span className="player-symbol">{symbol}</span>
-        <button>save</button>
+        <button onClick={handleSetIsEditing}>
+          {isEditing ? "Save" : "Edit"}
+        </button>
       </span>
     </li>
   );
